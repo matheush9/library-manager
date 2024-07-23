d8ba2fd0e0d45c3e2f8e27ce532bdc3eaafa4dd0
Matheus Henrique <matheushenr941@gmail.com>
2024-07-23 13:24:18 -0300

    fix: change base/book model to work with spring

--- src/main/java/henrique/matheus/librarymanager/model/BaseModel.java
+++ src/main/java/henrique/matheus/librarymanager/model/BaseModel.java
@@ -1,9 +1,39 @@
 package henrique.matheus.librarymanager.model;
 
-import java.time.ZonedDateTime;
+import jakarta.persistence.*;
 
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@MappedSuperclass
 public class BaseModel {
-    protected String Id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected UUID Id;
     protected ZonedDateTime dateTimeCreated;
     protected ZonedDateTime dateTimeModified;
+
+    public UUID getId() {
+        return Id;
+    }
+
+    public void setId(UUID id) {
+        Id = id;
+    }
+
+    public ZonedDateTime getDateTimeCreated() {
+        return dateTimeCreated;
+    }
+
+    public void setDateTimeCreated(ZonedDateTime dateTimeCreated) {
+        this.dateTimeCreated = dateTimeCreated;
+    }
+
+    public ZonedDateTime getDateTimeModified() {
+        return dateTimeModified;
+    }
+
+    public void setDateTimeModified(ZonedDateTime dateTimeModified) {
+        this.dateTimeModified = dateTimeModified;
+    }
 }
--- src/main/java/henrique/matheus/librarymanager/model/BookModel.java
+++ src/main/java/henrique/matheus/librarymanager/model/BookModel.java
@@ -1,10 +1,42 @@
 package henrique.matheus.librarymanager.model;
 
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.ZonedDateTime;
 
-public class BookModel extends BaseModel {
+@Entity
+@Table(name="BOOKS")
+public class BookModel extends BaseModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     public String name;
-    public AuthorModel authorModel;
     public int pages;
     public ZonedDateTime dateTimeReleased;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ZonedDateTime getDateTimeReleased() {
+        return dateTimeReleased;
+    }
+
+    public void setDateTimeReleased(ZonedDateTime dateTimeReleased) {
+        this.dateTimeReleased = dateTimeReleased;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
 }
