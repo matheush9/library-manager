49196451c667433632808d87e180bf98b21f9328
Matheus Henrique <matheushenr941@gmail.com>
2024-07-22 22:28:32 -0300

    feat: add spring app properties and boot start

--- src/main/java/henrique/matheus/librarymanager/LibraryApp.java
+++ src/main/java/henrique/matheus/librarymanager/LibraryApp.java
@@ -1,7 +1,11 @@
 package henrique.matheus.librarymanager;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class LibraryApp {
     public static void main(String[] args) {
-
+        SpringApplication.run(LibraryApp.class, args);
     }
 }
